5bf17498350e035919a7273daa80af0289409b42
AJ Wagner <ajwagner@northwesternmutual.com>
2017-05-04 16:12:03 -0500

    end of day May 4

--- Practice Work/functions.js
+++ Practice Work/functions.js
@@ -22,16 +22,64 @@ function fibonacci(z) {
 // an order list - arr a item to search for
 //return the arr index to where the item is found
 function divideNconquer(arr, searchNm) {
-    var searchFound = false;
-    var searchIndex = arr.length / 2;
+    var start = 0;
+    var endPt = arr.length - 1;
+    var midIndex;
 
-    while (!searchFound) {
-        if (searchNm == arr[searchIndex]) {
-            return searchIndex;
-        } else if (searchNm > arr[searchIndex]) {
-            searchIndex += searchIndex / 2;
+    while (start <= endPt) {
+        midIndex = Math.floor((start + endPt) / 2);
+
+        if (searchNm == arr[midIndex]) {
+            return midIndex;
+        } else if (searchNm < arr[midIndex]) {
+            endPt = midIndex - 1;
         } else {
-            searchIndex -= searchIndex / 2;
+            start = midIndex + 1;
+        }
+    }
+    console.log(`Search is out of range`)
+    return -1;
+}
+
+function nthLargest(arr, fromEnd) {
+    if (fromEnd > arr.length) {
+        return console.log(`position from end is greater than the length of the array provide`);
+    }
+
+    var tempArr = arr.sort();
+    var counter = 1;
+
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if (counter == fromEnd) {
+            return arr[i];
+        } else {
+            counter++;
         }
     }
 }
+
+console.log(nthLargest([1, 30, 20, 40, 0, 3], 2));
+
+function nthLargest(arr, fromEnd) {
+    if (fromEnd > arr.length) {
+        return console.log(`position from end is greater than the length of the array provide`);
+    }
+
+    var currentLargest;
+    var largestIndex;
+
+    for (var c = 0; c < fromEnd; c++) {
+        currentLargest = arr[0];
+        largestIndex = 0;
+
+        for (var i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > currentLargest) {
+                currentLargest = arr[i];
+                largestIndex = i;
+            }
+        }
+        arr[largestIndex] = null;
+    }
+    return currentLargest;
+}
+console.log(nthLargest([1, 30, 20, 40, 0, 3], 3));
